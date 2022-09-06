1cdeef4bcb7d928c68c1c4c2e29f245fc6fb3912
Atul <atulsourabh083@gmail.com>
2022-09-06 15:59:32 +0530

    Added Navbar

---
+++ Project 2/script.js
@@ -0,0 +1,10 @@
+
+console.log("sdsjvgc");
+let menu = document.querySelector('.navbar .max-width .menu');
+
+let list = document.querySelector(".navbar .max-width ul");
+
+menu.addEventListener('click', () =>{
+    list.classList.toggle("show");
+    // console.log("we");
+});
